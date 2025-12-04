071d7ed8eb63eac9733dea6be4e1955975af2934
Kosi07 <kosiossai07@gmail.com>
2025-12-04 12:37:55 +0100

    removed style on footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@
 const Footer = () => {
   return (
     <footer
-          className='fixed bottom-0 bg-gray-100 w-19/20 rounded-lg text-center md:text-2xl lg:text-3xl xl:text-4xl'>
+          className='text-center md:text-2xl lg:text-3xl xl:text-4xl'>
       &copy; {new Date().getFullYear()} ToDo List
     </footer>
   )
